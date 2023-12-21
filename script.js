abf67f6ef0723f502a2ceab574476080c9a6837f
Gregory2786 <114622096+Gregory2786@users.noreply.github.com>
2023-12-20 21:43:01 -0300

    summary

--- script.js
+++ script.js
@@ -3,44 +3,57 @@ document.addEventListener('DOMContentLoaded', function () {
   
     const memoryGame = document.getElementById('memoryGame');
   
-    // Duplicate the card values to create pairs
-    const cards = [...cardValues, ...cardValues];
-  
-    // Shuffle the cards using Fisher-Yates algorithm
-    for (let i = cards.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [cards[i], cards[j]] = [cards[j], cards[i]];
-    }
-  
-    // Create HTML elements for each card
-    cards.forEach((value, index) => {
-      const card = document.createElement('div');
-      card.classList.add('memory-card');
-      card.dataset.value = value;
-  
-      const cardInner = document.createElement('div');
-      cardInner.classList.add('card-inner');
-  
-      const faceFront = document.createElement('div');
-      faceFront.classList.add('face', 'front');
-      faceFront.textContent = '?';
-  
-      const faceBack = document.createElement('div');
-      faceBack.classList.add('face', 'back');
-      faceBack.textContent = value;
-  
-      cardInner.appendChild(faceFront);
-      cardInner.appendChild(faceBack);
-      card.appendChild(cardInner);
-  
-      card.addEventListener('click', flipCard);
-  
-      memoryGame.appendChild(card);
-    });
-  
+    let cards = [];
     let hasFlippedCard = false;
     let lockBoard = false;
     let firstCard, secondCard;
+    let moves = 0;
+    let playerName;
+  
+    // Função para criar as cartas
+    function createCards() {
+      cards = [...cardValues, ...cardValues];
+      cards = cards.sort(() => Math.random() - 0.5);
+  
+      cards.forEach((value, index) => {
+        const card = document.createElement('div');
+        card.classList.add('memory-card');
+        card.dataset.value = value;
+  
+        const cardInner = document.createElement('div');
+        cardInner.classList.add('card-inner');
+  
+        const faceFront = document.createElement('div');
+        faceFront.classList.add('face', 'front');
+        faceFront.textContent = '?';
+  
+        const faceBack = document.createElement('div');
+        faceBack.classList.add('face', 'back');
+        faceBack.textContent = value;
+  
+        cardInner.appendChild(faceFront);
+        cardInner.appendChild(faceBack);
+        card.appendChild(cardInner);
+  
+        card.addEventListener('click', flipCard);
+  
+        memoryGame.appendChild(card);
+      });
+    }
+  
+    // Função para iniciar o jogo
+    function startGame() {
+      createCards();
+  
+      // Iniciando o contador de jogadas
+      moves = 0;
+  
+      // Perguntando ao jogador o nome antes de iniciar o jogo
+      playerName = prompt('Qual é o seu nome?') || 'Jogador';
+    }
+  
+    // Iniciando o jogo
+    startGame();
   
     function flipCard() {
       if (lockBoard) return;
@@ -62,11 +75,17 @@ document.addEventListener('DOMContentLoaded', function () {
       const isMatch = firstCard.dataset.value === secondCard.dataset.value;
   
       isMatch ? disableCards() : unflipCards();
+      incrementMoves();
     }
   
     function disableCards() {
       firstCard.removeEventListener('click', flipCard);
       secondCard.removeEventListener('click', flipCard);
+  
+      // Verificando se todas as cartas foram combinadas
+      if (document.querySelectorAll('.flipped').length === cards.length) {
+        showCongratulations();
+      }
   
       resetBoard();
     }
@@ -85,6 +104,15 @@ document.addEventListener('DOMContentLoaded', function () {
     function resetBoard() {
       [hasFlippedCard, lockBoard] = [false, false];
       [firstCard, secondCard] = [null, null];
+    }
+  
+    function incrementMoves() {
+      moves++;
+    }
+  
+    function showCongratulations() {
+      const congratsMessage = `Parabéns, ${playerName}! Você completou o jogo em ${moves} jogadas.`;
+      alert(congratsMessage);
     }
   });
   
